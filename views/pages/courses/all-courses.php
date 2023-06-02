ed24755a5bdd1bb16fe4f013de6f714a7ce936b1
Md_Rabiul_Hasan <rhr2416@gmail.com>
2023-06-03 02:49:55 +0600

    pagination added

--- views/pages/courses/all-courses.php
+++ views/pages/courses/all-courses.php
@@ -3,6 +3,26 @@ session_start();
 $header_active = "Courses";
 include("../../partials/header.php");
 ?>
+<style>
+    nav.pagination-nav {
+        width: fit-content;
+        margin: auto;
+    }
+
+    a.page {
+        padding: 7px;
+        margin: 0 3px;
+        background: #dddddd;
+        font-size: 13px;
+        font-weight: bold;
+        border-radius: 4px;
+    }
+
+    .active-page {
+        color: white !important;
+        background-color: red !important;
+    }
+</style>
 <section class="featured-courses" id="featured-courses">
     <div class="container">
         <div class="row">
@@ -11,7 +31,14 @@ include("../../partials/header.php");
             </div>
             <div class="col-12 all-couses">
                 <?php
-                $courseSql = "SELECT `id`,`image`,`title`,`sub_title`,`instructor_id`,`price` FROM `courses`";
+                if (isset($_REQUEST['page'])) {
+                    $page = $_REQUEST['page'];
+                } else {
+                    $page = 1;
+                }
+                $per_page = 8;
+                $star_from = ($page - 1) * $per_page;
+                $courseSql = "SELECT `id`,`image`,`title`,`sub_title`,`instructor_id`,`price` FROM `courses` ORDER BY id DESC limit $star_from,$per_page";
                 $courseStmt = fetch_data($connection, $courseSql);
                 if ($courseStmt) {
                     mysqli_stmt_bind_result($courseStmt, $id, $image, $title, $subTitle, $instructorId, $price);
@@ -22,8 +49,7 @@ include("../../partials/header.php");
                         <a class="col-md-3 course-details-link mb-4" href="course-details/<?= $id; ?>">
                             <div class="course-item">
                                 <div class="card">
-                                    <img class="img-fluid" style="min-height: 150px" alt="100%x280"
-                                        src="<?= IMAGEPATH, $image; ?>" />
+                                    <img class="img-fluid" style="min-height: 150px" alt="100%x280" src="<?= IMAGEPATH, $image; ?>" />
                                     <div class="card-body">
                                         <h4 class="card-title">
                                             <?= $title; ?>
@@ -48,15 +74,68 @@ include("../../partials/header.php");
                                                     <?= $price; ?> ৳
                                                 </h4>
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
-                            </a>
-                        <?php }
-                    }
-                } ?>
+                            </div>
+                        </a>
+            <?php }
+                                    }
+                                } ?>
             </div>
-            <a href="<?= LINK; ?>courses" class="btn btn-success slide-btn news-btn">VIEW MORE</a>
+            <?php
+            $query =  "SELECT `id` FROM `courses`";
+            $result = fetch_data($connection, $query);
+            $total_rows = mysqli_stmt_num_rows($result);
+            $total_page = ceil($total_rows / $per_page);
+
+            $link = 5;
+            $start = (($page - $link) > 0) ? $page - $link : 1;
+            $last = (($page + $link) < $total_page) ? $page + $link : $total_page;
+
+            $prev_class = ($page == 1) ? 'disabled' : '';
+            $last_class = ($page == $total_page) ? 'disabled' : '';
+
+
+            echo '
+            <nav class="pagination-nav" aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item ' . $prev_class . '">
+                            <a class="page-link" href="' . LINK . 'courses/' . ($page - 1) . '" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+            ';
+
+            if ($start > 1) {
+                echo '
+                    <li class="page-item"><a class="page-link" href="' . LINK . 'courses/1">1</a></li>
+                    <li class="disabled"><span>...</span></li>
+                    ';
+            }
+            for ($i = $start; $i <= $last; $i++) {
+                $activePage = ($i == $page) ? "active-page" : "";
+                echo '
+                        <li class="page-item"><a class="page-link ' . $activePage . '" href="' . LINK . 'courses/' . $i . '">' . $i . '</a></li>
+                ';
+            }
+            if ($last < $total_page) {
+                echo '
+                <li class="disabled"><span>...</span></li>
+                <li class="page-item"><a class="page-link" href="' . LINK . 'courses/' . $total_page . '">' . $total_page . '</a></li>
+                ';
+            }
+            echo '
+                        <li class="page-item ' . $last_class . '">
+                            <a class="page-link" href="' . LINK . 'courses/' . ($page + 1) . '" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            ';
+            ?>
         </div>
     </div>
 </section>
